c734efc62022d88b965dd5bd74712805159924ea
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2022-11-02 22:17:21 +0900

    feat: add endpoints for GET all names, GET all rest times, GET a user
    Co-authored-by: kristinetuazon chadgrover <kristinetuazon chadgrover@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const db = require('../db/knex')
+const db = require('../db/knex');
 
 function setupServer() {
   const app = express();
@@ -9,8 +9,9 @@ function setupServer() {
   app.use(express.json());
 
   app.get("/", (req, res) => {
-    res.status(200).send("banana🍌");
+    res.status(200).send("anything you're looking for in particular? in the meantime, here's a banana🍌");
   });
+//NAMES
   //get all users 
   app.get("/users", async (req, res) => {
     try {
@@ -25,21 +26,71 @@ function setupServer() {
     }
   })
 
+  //get all names only
+  app.get("/users/name", async (req, res) => {
+    try {
+        const users = await db('user_table')
+            .select('first_name', 'last_name')
+            .timeout(1500);
+            users.length > 0
+            ? res.status(200).send(users)
+            : res.status(404).send("sorry, no users found")
+    } catch(err) {
+        res.status(500).send(err);
+    }
+  })
+
   //get all study times
-  app.get("/times_studied", async (req, res) => {
+  app.get("/time/study", async (req, res) => {
     try {
         const times = await db('study_time_table')
             .select('*')
             .timeout(1500);
             times.length > 0
             ? res.status(200).send(times)
-            : res.status(404).send("no times found")
+            : res.status(404).send("sorry, no times found")
     } catch(err) {
         res.status(500).send(err);
     }
   })
 
+  //get all rest times
+  app.get("/time/rest", async (req, res) => {
+    try {
+        const times = await db('study_time_table')
+            .select('*')
+            .timeout(1500);
+            times.length > 0
+            ? res.status(200).send(times)
+            : res.status(404).send("sorry, no times found")
+    } catch(err) {
+        res.status(500).send(err);
+    }
+  })
 
+  //add new user
+  app.post("/users/new", async (req, res) => {
+    const newUser = req.body;
+    const users = await db('user_table').select('*');
+    db.insert(req.body).returning('*').into()
+    console.log(req.body);
+    res.status(201).send(users);
+  })
+
+//get a particular user
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const users = await db('user_table').select('*');
+    console.log(users[id]);
+    res.status(200).send(users[id])
+})
+//LEADERBOARD
+  //get names
+  //get levels
+  //get experience
+
+  //add points for every study session.
+  app.patch("/level")
   return app;
 }
 
