d34d53957581b130602d57689c016b0a79e1e159
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-11-03 14:06:04 +0900

    fix: authProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Pomodoro from "./Pomodoro";
+import Login from "./components/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
