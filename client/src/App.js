dffe7b000532551238bfccbed9e197d98f5bac86
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-11-03 19:22:05 +0900

    feat added routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,20 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Pomodoro from "./Pomodoro";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./components/Login"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="app">
       <Pomodoro />
+      <BrowserRouter>
+        <Routes>
+          <Route path = {'/login'} element = {<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
