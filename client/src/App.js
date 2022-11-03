e68bfec4e38a90ecdfd91d3fbddc5c030debe284
Chad Grover <chadgrover@Chads-MacBook-Air.local>
2022-11-03 16:34:09 +0900

    feat: implement duration counter in Pomodoro
    Co-authored-by: kristinetuazon <kristinetuazon@users.noreply.github.com>
    Co-authored-by: JYYasuhiro <jyyasuhiro@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import Pomodoro from "./Pomodoro";
+import Pomodoro from "./components/Pomodoro";
 
 function App() {
   return (
