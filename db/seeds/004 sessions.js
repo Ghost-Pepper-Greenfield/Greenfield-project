03268e2a01fd6d00428ad0432877f4664f5f0e54
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2022-11-05 21:20:31 +0900

    fix: drop study_rest column and modified seeds
    Co-authored-by: kristinetuazon chadgrover <kristinetuazon chadgrover@users.noreply.github.com>

--- db/seeds/004 sessions.js
+++ db/seeds/004 sessions.js
@@ -6,14 +6,14 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('sessions_table').del()
   await knex('sessions_table').insert([
-    {id: 1, firebaseId: 'bobsmith123', date:'2022-11-01', duration:'220', study_rest:'true' , points: 100},
-    {id: 2, firebaseId: 'abcz57', date:'2022-11-01', duration:'12330', study_rest:'false', points: 10 },
-    {id: 3, firebaseId: 'xyz_person', date:'2022-11-02', duration:'13400', study_rest:'true', points: 120 },
-    {id: 4, firebaseId: 'bobsmith123', date:'2022-10-04', duration:'12200', study_rest:'true', points: 100 },
-    {id: 5, firebaseId: 'bobsmith123', date:'2022-11-02', duration:'2000', study_rest:'false', points: 200 },
-    {id: 6, firebaseId: 'abcz57', date:'2022-11-04', duration:'4000', study_rest:'true', points: 1000 },
-    {id: 7, firebaseId: 'abcz57', date:'2022-11-01', duration:'4000', study_rest:'false', points: 50 },
-    {id: 8, firebaseId: 'xyz_person', date:'2022-10-28', duration:'2400', study_rest:'true', points: 60 },
-    {id: 9, firebaseId: 'bobsmith123', date:'2022-10-28', duration:'3000', study_rest:'true', points: 300 },
+    {id: 1, firebaseId: 'bobsmith123', date:'2022-11-01', duration:'220' , points: 100},
+    {id: 2, firebaseId: 'abcz57', date:'2022-11-01', duration:'12330', points: 10 },
+    {id: 3, firebaseId: 'xyz_person', date:'2022-11-02', duration:'13400', points: 120 },
+    {id: 4, firebaseId: 'bobsmith123', date:'2022-10-04', duration:'12200', points: 100 },
+    {id: 5, firebaseId: 'bobsmith123', date:'2022-11-02', duration:'2000', points: 200 },
+    {id: 6, firebaseId: 'abcz57', date:'2022-11-04', duration:'4000', points: 1000 },
+    {id: 7, firebaseId: 'abcz57', date:'2022-11-01', duration:'4000', points: 50 },
+    {id: 8, firebaseId: 'xyz_person', date:'2022-10-28', duration:'2400', points: 60 },
+    {id: 9, firebaseId: 'bobsmith123', date:'2022-10-28', duration:'3000', points: 300 },
   ]);
 };
