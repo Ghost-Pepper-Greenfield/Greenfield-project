6781741035dc0d5de25971e5417393dd7558e650
Chad Grover <chadgrover@Chads-MacBook-Air.local>
2022-11-05 21:04:54 +0900

    fix: implement study_rest key in PostObject
    Co-authored-by: kristinetuazon <kristinetuazon@users.noreply.github.com>
    Co-authored-by: JYYasuhiro <jyyasuhiro@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -11,17 +11,23 @@ function setupServer() {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(express.json());
 
-  // app.get('/*', function(req, res) {
-  //   res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
-  //     if (err) {
-  //       res.status(500).send(err)
-  //     }
-  //   })
-  // })
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
-  app.get("/", (req, res) => {
-    res.status(200).send("anything you're looking for in particular? in the meantime, here's a banana🍌");
-  });
+
+  app.post('/test', (req, res) => {
+    console.log(req.body)
+    res.status(200).send('message received')
+  })
+
+  // app.get("/", (req, res) => {
+  //   res.status(200).send("anything you're looking for in particular? in the meantime, here's a banana🍌");
+  // });
 
   app.get('/sessions', async (req,res) => {
     try{
@@ -47,18 +53,20 @@ function setupServer() {
  
   //POST a time onto log of all sessions
   app.post('/new-session', async (req, res) => {
-    try{
+    // console.log("Test");
+    // res.status(201).send("Success");
+   try{
       const payload = req.body;
+      // change variable: table
       const table = await db('sessions_table')
-        .select('*')
+        .select('*') //may not be needed
         .insert(payload);
-        console.log(table);
-      res.status(201).send(payload);
+      console.log("Completed");
+      res.status(201).send("Success");
     } catch(err) {
       res.status(500).send(err);
     }
-    }
-  )
+  })
 
 //LEADERBOARD
   app.get('/leaderboard', async (req, res) => {
