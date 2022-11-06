5c5aebe032b0679d74d65f0cc7b3dbf364f22306
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2022-11-06 09:57:14 +0900

    feat: add leaderboard component
    Co-authored-by: kristinetuazon chadgrover <kristinetuazon chadgrover@users.noreply.github.com>

--- client/src/components/LeaderboardLog.js
+++ client/src/components/LeaderboardLog.js
@@ -1,10 +1,28 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function LeaderboardLog() {
+  const[ leaderboard, setLeaderboard ] = useState();
+  const chart = leaderboard.map( player => {
+    return <p>Player:{ player.firebaseId } Level:{ player.sum }</p>
+  });
+
+  useEffect(() => {
+    getLeaderboard();
+  }, [])
+
+  async function getLeaderboard() {
+    try{
+      const fetchLeaderboard = await axios.get('/leaderboard');
+      setLeaderboard(fetchLeaderboard.data);
+    }catch(err) {
+    console.log(err)
+    alert("An error occurred while fetching leaderboard data");
+   } 
+  }
   return (
-    <div>LeaderboardLog</div>
+    <>
+     <div>{chart}</div>
+    </>
   )
 }
