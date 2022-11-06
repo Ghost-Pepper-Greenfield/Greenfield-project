680f3c5ea47e155f9a11bf8e7e047e9977759b2f
Chad Grover <chadgrover@Chads-MacBook-Air.local>
2022-11-06 09:04:32 +0900

    feat: implement CSS changes in Pomodoro and Study
    Co-authored-by: kristinetuazon <kristinetuazon@users.noreply.github.com>
    Co-authored-by: JYYasuhiro <jyyasuhiro@users.noreply.github.com>

--- client/src/components/Study.js
+++ client/src/components/Study.js
@@ -1,9 +1,10 @@
+import "../styles/study.css";
 import React from 'react'
 import Pomodoro from "./Pomodoro"
 
 export default function Study() {
   return (
-    <div>
+    <div id= "study__wrapper">
       <Pomodoro />
     </div>
   )
